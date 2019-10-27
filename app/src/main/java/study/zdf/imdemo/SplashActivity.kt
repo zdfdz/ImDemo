7e667952ac2796ba875cc282a5e51d4fc90a6d4e
zdfdz <zdfdz@outlook.com>
2019-10-27 16:19:28 +0800

    # splash页面

---
+++ app/src/main/java/study/zdf/imdemo/SplashActivity.kt
@@ -0,0 +1,12 @@
+package study.zdf.imdemo
+
+/**
+ * @author ZhengDeFeng
+ * @description:
+ * @date :2019/10/27 12:12
+ */
+class SplashActivity:BaseActivity() {
+    override fun getLayoutResID(): Int {
+        return R.layout.activity_splash
+    }
+}
