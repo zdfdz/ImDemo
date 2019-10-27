22657a661f272d52b5ea5470a350f61c65be769a
zdfdz <zdfdz@outlook.com>
2019-10-27 16:48:21 +0800

    # splash的View的实现完成

---
+++ app/src/main/java/study/zdf/imdemo/LoginActivity.kt
@@ -0,0 +1,12 @@
+package study.zdf.imdemo
+
+/**
+ * @author ZhengDeFeng
+ * @description:
+ * @date :2019/10/27 16:41
+ */
+class LoginActivity :BaseActivity() {
+    override fun getLayoutResID(): Int {
+         return R.layout.activity_login
+    }
+}
--- app/src/main/java/study/zdf/imdemo/SplashActivity.kt
+++ app/src/main/java/study/zdf/imdemo/SplashActivity.kt
@@ -1,12 +1,39 @@
 package study.zdf.imdemo
 
+import android.os.Handler
+import org.jetbrains.anko.startActivity
+import study.zdf.imdemo.contract.SplashContract
+
 /**
  * @author ZhengDeFeng
  * @description:
  * @date :2019/10/27 12:12
  */
-class SplashActivity:BaseActivity() {
+class SplashActivity : BaseActivity(), SplashContract.View {
+
+    //伴生对象里都是静态的
+    companion object {
+        val DELAY: Long = 2000
+    }
+
+    private val mHandler by lazy { Handler() }
+
     override fun getLayoutResID(): Int {
         return R.layout.activity_splash
+    }
+
+    // 显示两秒，跳转到登录界面
+    override fun onNotLoggedIn() {
+        mHandler.postDelayed(object : Runnable {
+            override fun run() {
+                startActivity<LoginActivity>()
+                finish()
+            }
+        }, DELAY)
+    }
+
+    //跳转到主界面
+    override fun onLoggedIn() {
+        startActivity<MainActivity>()
     }
 }
