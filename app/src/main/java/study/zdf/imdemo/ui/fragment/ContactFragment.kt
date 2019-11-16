2aaae4c3223d304249034779429891619a904480
zdfdz <zdfdz@outlook.com>
2019-11-16 23:33:45 +0800

    #联系人布局初始化

--- app/src/main/java/study/zdf/imdemo/ui/fragment/ContactFragment.kt
+++ app/src/main/java/study/zdf/imdemo/ui/fragment/ContactFragment.kt
@@ -1,5 +1,8 @@
 package study.zdf.imdemo.ui.fragment
 
+import android.view.View
+import kotlinx.android.synthetic.main.fragment_contacts.*
+import kotlinx.android.synthetic.main.header.*
 import study.zdf.imdemo.R
 
 /**
@@ -7,8 +10,18 @@ import study.zdf.imdemo.R
  * @description:
  * @date :2019/11/7 23:16
  */
-class ContactFragment :BaseFragment() {
+class ContactFragment : BaseFragment() {
     override fun getLayoutResID(): Int {
         return R.layout.fragment_contacts
+    }
+
+    override fun init() {
+        headerTitle.text = getString(R.string.contact)
+        add.visibility = View.VISIBLE
+
+        swipeRefreshLayout.apply {
+            setColorSchemeResources(R.color.colorPrimary)
+            isRefreshing = true
+        }
     }
 }
