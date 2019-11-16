2fdb4e7bf316ad455333c010ac686951c83af034
zdfdz <zdfdz@outlook.com>
2019-11-16 20:50:26 +0800

    #退出逻辑完成

--- app/src/main/java/study/zdf/imdemo/ui/fragment/DynamicFragment.kt
+++ app/src/main/java/study/zdf/imdemo/ui/fragment/DynamicFragment.kt
@@ -3,7 +3,12 @@ package study.zdf.imdemo.ui.fragment
 import com.hyphenate.chat.EMClient
 import kotlinx.android.synthetic.main.fragment_dynamic.*
 import kotlinx.android.synthetic.main.header.*
-import study.zdf.imdemo.R
+import com.hyphenate.EMCallBack
+import org.jetbrains.anko.runOnUiThread
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+import study.zdf.imdemo.ui.activity.LoginActivity
+
 
 /**
  * @author ZhengDeFeng
@@ -12,13 +17,43 @@ import study.zdf.imdemo.R
  */
 class DynamicFragment :BaseFragment() {
     override fun getLayoutResID(): Int {
-        return R.layout.fragment_dynamic
+        return study.zdf.imdemo.R.layout.fragment_dynamic
     }
 
     override fun init() {
         super.init()
-        headerTitle.text = getString(R.string.dynamic)
-        val loginString = String.format(getString(R.string.logout),EMClient.getInstance().currentUser)
+        headerTitle.text = getString(study.zdf.imdemo.R.string.dynamic)
+        val loginString = String.format(getString(study.zdf.imdemo.R.string.logout),EMClient.getInstance().currentUser)
         logout.text = loginString
+
+        logout.setOnClickListener {
+            exitLogin()
+        }
     }
+
+    private fun exitLogin() {
+        EMClient.getInstance().logout(true, object : EMCallBack {
+
+            override fun onSuccess() {
+                context?.startActivity<LoginActivity>()
+                activity?.finish()
+
+            }
+
+            override fun onProgress(progress: Int, status: String) {
+
+
+            }
+
+            override fun onError(code: Int, message: String) {
+            //切换到主线程
+                context?.runOnUiThread {
+                    toast("退出失败")
+                }
+
+            }
+        })
+    }
+
+
 }
