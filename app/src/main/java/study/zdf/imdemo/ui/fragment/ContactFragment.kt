625f7eea8ad18fa4a9a39a436e96c722ce9e92b2
zdfdz <zdfdz@outlook.com>
2019-11-17 17:30:36 +0800

    #联系人View层实现

--- app/src/main/java/study/zdf/imdemo/ui/fragment/ContactFragment.kt
+++ app/src/main/java/study/zdf/imdemo/ui/fragment/ContactFragment.kt
@@ -2,17 +2,21 @@ package study.zdf.imdemo.ui.fragment
 
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_contacts.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.toast
 import study.zdf.imdemo.R
 import study.zdf.imdemo.adapter.ContactListAdapter
+import study.zdf.imdemo.contract.ContactContract
 
 /**
  * @author ZhengDeFeng
  * @description:
  * @date :2019/11/7 23:16
  */
-class ContactFragment : BaseFragment() {
+class ContactFragment : BaseFragment(), ContactContract.View {
+
     override fun getLayoutResID(): Int {
         return R.layout.fragment_contacts
     }
@@ -30,7 +34,17 @@ class ContactFragment : BaseFragment() {
             //当确定recyclerView的内部布局大小不会再改变的时候设置为true，会有内部优化
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(context)
-            adapter=ContactListAdapter()
+            adapter = ContactListAdapter()
         }
+    }
+
+    override fun onLoadContactsSuccess() {
+        swipeRefreshLayout.isRefreshing = false
+        recyclerView.adapter?.notifyDataSetChanged()
+    }
+
+    override fun onLoadContactsFailed() {
+        swipeRefreshLayout.isRefreshing = false
+        context?.toast("刷新失败")
     }
 }
