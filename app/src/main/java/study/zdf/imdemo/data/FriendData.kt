27d189af71e94bbf0b420649632f3e8d6c8a6f05
zdfdz <zdfdz@outlook.com>
2019-12-14 21:00:54 +0800

    数据库表的定义

---
+++ app/src/main/java/study/zdf/imdemo/data/FriendData.kt
@@ -0,0 +1,9 @@
+package study.zdf.imdemo.data
+
+/**
+ * @author ZhengDeFeng
+ * @description:
+ * @date :2019/12/14 20:55
+ */
+class FriendData {
+}
---
+++ app/src/main/java/study/zdf/imdemo/data/db/Tables.kt
@@ -0,0 +1,15 @@
+package study.zdf.imdemo.data.db
+
+/**
+ * @author ZhengDeFeng
+ * @description: 可以直接用 类名 访问
+ * @date :2019/12/14 20:55
+ */
+object Contact {
+    // 表名
+    val NAME = "contact"
+
+    // 字段
+    val ID = "_id"
+    val CONTACT = "name"
+}
